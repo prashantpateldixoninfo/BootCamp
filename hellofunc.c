eb5660ac0fb5c4075f49516028ba6da50eb0194b
rohit <rohit.kumar@dixon@gmail.com>
2023-01-25 11:46:56 +0000

    Changes done based on review comment

--- hellofunc.c
+++ hellofunc.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <hellomake.h>
 
-void myPrintHelloMake(void) {
-
-	for(int i=0; i<5; i++)
-	{
-      	  printf("Hello makefiles!\n");
-	}
-
-  return;
+void myPrintHelloMake(void)
+{
+    for(int i=0; i<5; i++)
+    {
+        printf("Hello makefiles!\n");
+    }
 }
